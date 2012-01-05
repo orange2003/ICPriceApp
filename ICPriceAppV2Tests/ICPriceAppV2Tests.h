b0141255e96c102d32ff418a2c61a8209f4f281a
高飞 <gaofei2003@gmail.com>
2012-01-05 14:23:04 +0800

    Initial Commit

---
+++ ICPriceAppV2Tests/ICPriceAppV2Tests.h
@@ -0,0 +1,13 @@
+//
+//  ICPriceAppV2Tests.h
+//  ICPriceAppV2Tests
+//
+//  Created by 高飞 on 12-1-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ICPriceAppV2Tests : SenTestCase
+
+@end
---
+++ ICPriceAppV2Tests/ICPriceAppV2Tests.m
@@ -0,0 +1,32 @@
+//
+//  ICPriceAppV2Tests.m
+//  ICPriceAppV2Tests
+//
+//  Created by 高飞 on 12-1-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ICPriceAppV2Tests.h"
+
+@implementation ICPriceAppV2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ICPriceAppV2Tests");
+}
+
+@end
